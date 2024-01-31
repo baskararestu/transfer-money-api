40fe87ab74ce8019ba07559715544c0117adf78e
baskara <baskararw10@gmail.com>
2024-01-31 22:22:56 +0700

    [FEAT] transfer money between user account of bank

--- controllers/transactionController/transactionController.go
+++ controllers/transactionController/transactionController.go
@@ -36,3 +36,36 @@ func Deposit(c *gin.Context) {
 
 	c.JSON(http.StatusOK, response)
 }
+
+func Transfer(c *gin.Context) {
+	var req struct {
+		ReceiverAccountNumber string  `json:"receiver_account_number"`
+		Amount                float64 `json:"amount"`
+	}
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, errorresponse.NewErrorResponse(err.Error()))
+		return
+	}
+
+	result := services.TransferService(c, req.ReceiverAccountNumber, req.Amount)
+	if result.Error != nil {
+		c.JSON(http.StatusInternalServerError, errorresponse.NewErrorResponse(result.Error.Error()))
+		return
+	}
+
+	transferData := transactionresponse.TransferData{
+		Amount:       req.Amount,
+		FromAccount:  result.Data.FromAccount,
+		SenderName:   result.Data.SenderName,
+		ToAccount:    req.ReceiverAccountNumber,
+		ReceiverName: result.Data.ReceiverName,
+	}
+
+	response := transactionresponse.TransferResponse{
+		Message: "Transfer successful",
+		Success: true,
+		Data:    transferData,
+	}
+
+	c.JSON(http.StatusOK, response)
+}
--- services/transactionService.go
+++ services/transactionService.go
@@ -88,6 +88,111 @@ func getBankAccountByUserID(tx *gorm.DB, userID uuid.UUID) (*models.BankAccount,
 	return &bankAccount, nil
 }
 
+func TransferService(c *gin.Context, receiverAccountNumber string, amount float64) transactionresponse.TransferServiceResult {
+	userID, err := middlewares.GetUserIDFromContext(c)
+	if err != nil {
+		return transactionresponse.TransferServiceResult{Error: err}
+	}
+
+	tx := database.DB.Begin()
+
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+		}
+	}()
+
+	senderAccount, err := getBankAccountByUserID(tx, userID)
+	if err != nil {
+		tx.Rollback()
+		return transactionresponse.TransferServiceResult{Error: fmt.Errorf("failed to get sender's bank account: %v", err)}
+	}
+
+	senderUser, err := GetUserByID(userID.String())
+	if err != nil {
+		tx.Rollback()
+		return transactionresponse.TransferServiceResult{Error: fmt.Errorf("failed to get sender's user data: %v", err)}
+	}
+	senderName := senderUser.FullName
+
+	receiverAccount, err := getBankAccountByAccountNumber(tx, receiverAccountNumber)
+	if err != nil {
+		tx.Rollback()
+		return transactionresponse.TransferServiceResult{Error: fmt.Errorf("failed to get receiver's bank account: %v", err)}
+	}
+
+	if senderAccount.Balance < amount {
+		tx.Rollback()
+		return transactionresponse.TransferServiceResult{Error: errors.New("insufficient balance")}
+	}
+
+	senderAccount.Balance -= amount
+	if err := tx.Save(&senderAccount).Error; err != nil {
+		tx.Rollback()
+		return transactionresponse.TransferServiceResult{Error: fmt.Errorf("failed to update sender's bank account balance: %v", err)}
+	}
+
+	receiverAccount.Balance += amount
+	if err := tx.Save(&receiverAccount).Error; err != nil {
+		tx.Rollback()
+		return transactionresponse.TransferServiceResult{Error: fmt.Errorf("failed to update receiver's bank account balance: %v", err)}
+	}
+
+	// Retrieve receiver's name
+	receiverName := ""
+	if err := tx.Model(&receiverAccount.User).Select("full_name").Scan(&receiverName).Error; err != nil {
+		tx.Rollback()
+		return transactionresponse.TransferServiceResult{Error: fmt.Errorf("failed to retrieve receiver's name: %v", err)}
+	}
+
+	if err := createTransferTransactions(tx, senderAccount, receiverAccount, amount); err != nil {
+		tx.Rollback()
+		return transactionresponse.TransferServiceResult{Error: fmt.Errorf("failed to create transaction records: %v", err)}
+	}
+
+	data := transactionresponse.TransferData{
+		Amount:       amount,
+		FromAccount:  senderAccount.AccountNumber,
+		ToAccount:    receiverAccount.AccountNumber,
+		ReceiverName: receiverName,
+		SenderName:   senderName,
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		return transactionresponse.TransferServiceResult{Error: fmt.Errorf("failed to commit transaction: %v", err)}
+	}
+
+	return transactionresponse.TransferServiceResult{
+		Success: true,
+		Message: "Transfer successful",
+		Data:    data,
+	}
+}
+
+func getBankAccountByAccountNumber(tx *gorm.DB, accountNumber string) (*models.BankAccount, error) {
+	var bankAccount models.BankAccount
+	if err := tx.Where("account_number = ?", accountNumber).First(&bankAccount).Error; err != nil {
+		return nil, err
+	}
+	return &bankAccount, nil
+}
+
+func createTransferTransactions(tx *gorm.DB, sender, receiver *models.BankAccount, amount float64) error {
+	setTransaction := models.Transaction{
+		ID:          uuid.New(),
+		AccountID:   sender.ID,
+		ToAccountID: receiver.ID,
+		Amount:      amount,
+		Type:        models.TransferTransaction,
+		Status:      "completed",
+	}
+
+	if err := createTransaction(tx, &setTransaction); err != nil {
+		return err
+	}
+
+	return nil
+}
 func createTransaction(tx *gorm.DB, transaction *models.Transaction) error {
 	if err := tx.Create(transaction).Error; err != nil {
 		return err
